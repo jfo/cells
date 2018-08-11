148e62afa21d0b1c5f156c991d20972c67a1243f
Jeff Fowler <jeffowler@gmail.com>
2018-08-11 22:07:32 +0200

    minimal vp

--- main.js
+++ main.js
@@ -1,32 +1,63 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const Cell = props => (
-  <div
-    style={{
-      display: 'inline-block',
-      height: '20px',
-      width: '20px',
-      backgroundColor: props.active ? 'pink' : 'blue',
-      border: '1px solid black',
-    }}
-  />
-);
+import {createStore} from 'redux';
+import {connect, Provider} from 'react-redux';
 
-const n = 60;
+const n = 80;
+
+const Cell = props => {
+  return (
+    <div
+      style={{
+        display: 'inline-block',
+        height: '15px',
+        width: '15px',
+        backgroundColor: props.active ? 'hotpink' : 'lightblue',
+        border: '1px dotted green',
+      }}
+      onClick={() => props.onCellClick(props.x, props.y)}
+    />
+  );
+};
+
+const CellContainer = connect(
+  (state, ownProps) => ({
+    active: state.x === ownProps.x && state.y === ownProps.y,
+  }),
+  dispatch => ({
+    onCellClick: (x, y) => {
+      dispatch({type: 'ON_CELL_CLICK', data: {x, y}});
+    },
+  }),
+)(Cell);
 
 const Row = props =>
   Array(n)
     .fill()
-    .map((_, i) => <Cell y={props.key} key={i} />);
+    .map((_, i) => <CellContainer y={props.y} x={i} key={i} />);
 
 const Table = props =>
   Array(n)
     .fill()
     .map((_, i) => (
-      <div>
-        <Row key={i} />
+      <div key={i}>
+        <Row y={i} />
       </div>
     ));
 
-ReactDOM.render(<Table />, document.getElementById('root'));
+const store = createStore((state = {x: 1, y: 1}, action) => {
+  switch (action.type) {
+    case 'ON_CELL_CLICK': {
+      return action.data;
+    }
+  }
+  return state;
+} /* , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() */ );
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Table />
+  </Provider>,
+  document.getElementById('root'),
+);
